818aa03442e5a66ba2741535b7b20d1914965c3f
YeisonDC <jeiisoncelis3@gmail.com>
2025-07-04 14:47:19 -0500

    Pedidos Admin1

--- Front/src/pages/TodosLosPedidos.jsx
+++ Front/src/pages/TodosLosPedidos.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import API from '../../api';
+import API from '../api';
 import './TodosLosPedidos.css'; // Puedes crear este archivo para estilos opcionales
 
 const TodosLosPedidos = () => {
