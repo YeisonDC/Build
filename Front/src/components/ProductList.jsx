f5308b93ecc8ca69b8170b2648b6a17338649c6f
YeisonDC <jeiisoncelis3@gmail.com>
2025-07-07 18:50:13 -0500

    Frontend

--- Front/src/components/ProductList.jsx
+++ Front/src/components/ProductList.jsx
@@ -3,14 +3,6 @@ import ProductCard from './ProductCard';
 import API from '../api';
 import './ProductList.css';
 import { FiFilter, FiChevronLeft, FiChevronRight } from 'react-icons/fi';
-import Masonry from 'react-masonry-css';
-
-const breakpointColumnsObj = {
-  default: 4,
-  1200: 3,
-  768: 2,
-  480: 1,
-};
 
 const ProductList = ({ initialCategory = null }) => {
   const [products, setProducts] = useState([]);
@@ -207,15 +199,9 @@ const ProductList = ({ initialCategory = null }) => {
 
         <main className="products-grid">
           {productosPagina.length > 0 ? (
-            <Masonry
-              breakpointCols={breakpointColumnsObj}
-              className="products-masonry-grid"
-              columnClassName="products-masonry-column"
-            >
-              {productosPagina.map(product => (
-                <ProductCard key={product._id} product={product} />
-              ))}
-            </Masonry>
+            productosPagina.map(product => (
+              <ProductCard key={product._id} product={product} />
+            ))
           ) : (
             <div className="no-products-message">
               No se encontraron productos que coincidan con los filtros seleccionados.
